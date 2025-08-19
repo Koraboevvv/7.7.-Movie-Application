a7d5e98d858ab2005de0c7e1ad38d2ab0af179a1
Koraboevvv <bexruzkarabaev@gmail.com>
2025-08-20 01:07:09 +0500

    run

--- src/shared/components/carousel/Carousel.tsx
+++ src/shared/components/carousel/Carousel.tsx
@@ -4,7 +4,7 @@ import { Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/Thumbs";
+import "swiper/css/thumbs";
 import { api } from "../../api";
 
 interface Movie {
